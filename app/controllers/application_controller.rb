cb42b103d2cd6677ceaf0e948830f5e5439b3223
vidoda <dauce@ua.pt>
2020-06-24 02:42:38 +0100

    erro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,6 @@ class ApplicationController < ActionController::Base
   { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 
-
-
-  @page_title       = 'Member Login'
-  @page_description = 'Member login page.'
-  @page_keywords    = 'Site, Login, Members'
-
-  set_meta_tags title: 'Member Login',
-              description: 'Member login page.',
-              keywords: 'Site, Login, Members'
-
-
 end
 
 
